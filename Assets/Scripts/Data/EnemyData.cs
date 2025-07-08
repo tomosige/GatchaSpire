fbf8aecbf8dae78959d9085966dd986426e49c85
tomosige <tomoterranite@gmail.com>
2025-07-08 09:20:46 +0900

    chore: Unityプロジェクトを初期化

--- Assets/Scripts/Data/EnemyData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemyData", menuName = "GatchaSpire/EnemyData")]
-public class EnemyData : CharacterData // CharacterDataを継承
-{
-    // 敵固有のプロパティがあればここに追加
-}
